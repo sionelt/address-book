3196ea2d161e516f1f355f59cfaf5702db1dd644
sionelt <sltafengatoto@gmail.com>
2019-07-11 10:42:41 -0600

    finish up home component when there's no people

--- client/src/components/home/Home.component.jsx
+++ client/src/components/home/Home.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Home = () => <div>Select a Contact</div>
+const Home = () => (
+  <div className="container m-auto d-flex justify-content-center align-items-center flex-column">
+    <FontAwesomeIcon icon="address-book" size="3x" className="text-primary" />
+    <h4 className="my-3 text-info">Currently No Contacts</h4>
+  </div>
+)
 
 export default Home
